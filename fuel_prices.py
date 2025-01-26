d9d8e08d324bde0d3be91d2314904d48523b5848
Pierre <pierre_kupka@web.de>
2025-01-26 14:22:31 +0100

    put code in an endless loop

--- fuel_prices.py
+++ fuel_prices.py
@@ -4,9 +4,13 @@ import logging
 import requests
 from time import sleep
 import re
+from random import randint
 
+# TODO : 
+# implement logger
+# get main data from ui conig ?! 
+# think about how to make the code available 
 
-# TODO : get data from ui conig ?!
 # set main data
 import mySecrets
 api_key = mySecrets.api_key
@@ -41,14 +45,16 @@ def discover_topic(station_name : str, fuel_type : str) -> str:
     "device_class": "monetary"
     })
 
-api_call = f"https://creativecommons.tankerkoenig.de/json/list.php?lat={lat_coords}&lng={lng_coords}&rad={radius}&sort=dist&type=all&apikey={api_key}"
-response = requests.get(api_call)
-response.encoding = 'utf-8'
-contents = response.json()
+# def get_fuel_prices():
+#     api_call = f"https://creativecommons.tankerkoenig.de/json/list.php?lat={lat_coords}&lng={lng_coords}&rad={radius}&sort=dist&type=all&apikey={api_key}"
+#     response = requests.get(api_call)
+#     response.encoding = 'utf-8'
+#     return response.json()
 
 # TODO : DELETE AFTER TESTING
-# with open('D:\Programmierung\tank_contents.md','r') as file:
-#     contents = json.loads(file.read())
+def get_fuel_prices():
+    with open(r'D:\Programmierung\tank_contents.md','r') as file:      # just a copy from the api for tests
+        return json.loads(file.read())
 
 def state_topic_value(fuel_price : float) -> str:
     return json.dumps({"value" : fuel_price})
@@ -59,22 +65,30 @@ def on_connect(client, userdata, flags, reason_code, properties=None):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.username_pw_set(username=broker_user, password=broker_pw)
-client.connect(host=broker_ip, port=1883, keepalive=60)
 
-client.loop_start()
+while True:
+    try:
+        client.connect(host=broker_ip, port=1883, keepalive=60)
+        contents = get_fuel_prices()
+        client.loop_start()
+        for station in contents.get('stations'):
+            # check if station is in wanted postcode
+            if station.get('postCode') in relevant_postcodes:
+                # send discover topic once
+                postcode = station.get('postCode')
+                station_name = station.get('name')
+                sensor_station_name = re.sub(r"[^a-zA-Z0-9_]", "_", str(postcode) + "_" + station_name.replace(" ", "_"))
 
-for station in contents.get('stations'):
-    # check if station is in wanted postcode
-    if station.get('postCode') in relevant_postcodes:
-        # send discover topic once
-        # discover_topic(station_name=str(postcode) + "_" + station_name)
-        postcode = station.get('postCode')
-        station_name = station.get('name')
-        sensor_station_name = re.sub(r"[^a-zA-Z0-9_]", "_", str(postcode) + "_" + station_name.replace(" ", "_"))
+                for fuel_type in fuel_types:
+                    client.publish(f"homeassistant/sensor/fuel_prices/{sensor_station_name + "_" + fuel_type}/config", discover_topic(sensor_station_name, fuel_type), 1 , True)
+                    client.publish(state_topic(sensor_station_name, fuel_type), state_topic_value(station.get(fuel_type)), 1,True)
 
-        for fuel_type in fuel_types:
-            client.publish(f"homeassistant/sensor/fuel_prices/{sensor_station_name + "_" + fuel_type}/config", discover_topic(sensor_station_name, fuel_type), 1 , True)
-            client.publish(state_topic(sensor_station_name, fuel_type), state_topic_value(station.get(fuel_type)), 1,True)
-
-client.loop_stop()
-client.disconnect()
+        client.loop_stop()
+        client.disconnect()
+        sleep_time = 1 * 60 + randint(5,58)
+        print(f"go to sleep for {sleep_time}s") # TODO : delete after testing
+        sleep(sleep_time) # respect traffic limits
+    except Exception as e:
+        print(e)
+        client.loop_stop()
+        client.disconnect()
